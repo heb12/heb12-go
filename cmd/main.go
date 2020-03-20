96ce73f3f6059a1f6c7907740f6375c97900b804
Ted Jameson <ted.jameson@pm.me>
2020-03-20 19:34:19 +0200

    Make CLI work

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,7 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"code.heb12.com/heb12/bref"
-	"code.heb12.com/heb12/heb12/config"
-	"code.heb12.com/heb12/heb12/osis"
+	"code.heb12.com/heb12/heb12/bible"
 )
 
 // printVerses prints all the verses with verse numbers
@@ -20,44 +19,67 @@ func printVerses(verses []string, ref bref.Reference) {
 }
 
 func main() {
-	// Initialize osistool
-	//osismanage, err := osistool.New(config.DataDirs.GratisSplit)
+	var translation string
 
 	app := &cli.App{
-		Name:  "heb12",
-		Usage: "Read the Bible",
+		Name:    "heb12",
+		Version: "v0.1.0",
+		Usage:   "Read the Bible",
 		Commands: []*cli.Command{
 			{
 				Name:    "read",
 				Aliases: []string{"r"},
 				Usage:   "Read a passage of scripture",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "translation",
+						Value:       "web",
+						Aliases:     []string{"t"},
+						Usage:       "Bible version to use",
+						Destination: &translation,
+					},
+				},
 				Action: func(c *cli.Context) error {
 					if c.Args().First() == "" {
 						fmt.Println("Usage example: aiodl read \"John 3:16\"")
 						return nil
 					}
 					ref := c.Args().First()
+					text, err := bible.Get(ref, translation)
+					if err != nil {
+						return err
+					}
+
 					reference, err := bref.Parse(ref)
 					if err != nil {
 						return err
 					}
-					scope := config.GetScope()
-					dataDir, err := scope.DataDir()
-					osisData, err := osis.Load(dataDir + "/" + config.DataDirs.GratisSplit + "/en/asv.xml")
-					if err != nil {
-						return err
-					}
-					text, err := osisData.GetVerses(osis.Reference{
-						ID:      reference.ID,
-						Chapter: reference.Chapter,
-						From:    reference.From,
-						To:      reference.To,
-					})
-					if err != nil {
-						return err
-					}
+
 					printVerses(text, reference)
 
+					return nil
+				},
+			},
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "List downloaded Bible translations",
+				Action: func(c *cli.Context) error {
+					translations, err := bible.List()
+					if err != nil {
+						return err
+					}
+					languages, err := bible.ListLanguages()
+					if err != nil {
+						return err
+					}
+
+					for _, lang := range languages {
+						fmt.Println(lang + ":")
+						for _, version := range translations[lang] {
+							fmt.Println("\t" + version)
+						}
+					}
 					return nil
 				},
 			},
